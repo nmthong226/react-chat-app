04d42a44669d953e304f80ef7697ba2efc99175f
nmthong226 <nmthong226@gmail.com>
2024-09-21 13:49:25 +0700

    feat(sidebar): add sidebar

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,35 @@
+import UserAvatar from "./UserAvatar"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faFileLines, faGear, faHome, faMessage, faTag } from '@fortawesome/free-solid-svg-icons';
+
+const SideBar = () => {
+  const navItems = [
+    { icon: faHome, label: 'Properties' },
+    { icon: faMessage, label: 'Chat', active: true },
+    { icon: faCalendar, label: 'Calendar' },
+    { icon: faTag, label: 'Offers' },
+    { icon: faFileLines, label: 'Documents' },
+    { icon: faGear, label: 'Settings' },
+  ];
+
+  return (
+    <aside className="flex flex-col w-64 h-screen bg-sidebarBg">
+      <UserAvatar className="mt-6 items-center" />
+      <nav>
+        <ul className="flex flex-col w-full gap-4 text-md mt-8 uppercase text-zinc-500 font-semibold">
+          {navItems.map(({ icon, label, active }) => (
+            <li
+              key={label}
+              className={`flex flex-row items-center space-x-6 py-2 px-8 hover:text-blue-600 hover:border-blue-600 hover:cursor-pointer border-l-4 border-l-transparent hover:border-l-blue-600 ${active ? 'text-blue-600 border-l-current border-l-blue-600' : ''}`}
+            >
+              <FontAwesomeIcon icon={icon} className="size-4" />
+              <span>{label}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default SideBar
