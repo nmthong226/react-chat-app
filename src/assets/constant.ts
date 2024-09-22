6f90103abc379bfe804b289bb73c2aceccb023d0
nmthong226 <nmthong226@gmail.com>
2024-09-22 17:29:41 +0700

    chore: add dummy data

---
+++ src/assets/constant.ts
@@ -0,0 +1,25 @@
+import userImg from '/user-avatar.png';
+
+export const userList = [
+    {
+        avatar: userImg, name: 'Rohit', isOnline: true, lastMessage: 'Hello', lastMessageTime: '21 Sep', unreadedMessages: 5
+    },
+    {
+        avatar: userImg, name: 'Iftikhar Shaikh', lastMessage: 'early prototype of product', lastMessageTime: '31 Mar', unreadedMessages: 1
+    },
+    {
+        avatar: userImg, name: 'Victor Erixon', isOnline: true, lastMessage: 'New project of you', lastMessageTime: '02 Feb', unreadedMessages: 3
+    },
+    {
+        avatar: userImg, name: 'Hali', lastMessage: 'Welcome', lastMessageTime: '31 Jan', unreadedMessages: 0
+    },
+    {
+        avatar: userImg, name: 'Jony Ive', isOnline: true, lastMessage: 'Hey', lastMessageTime: '12 Dec', unreadedMessages: 0
+    },
+    {
+        avatar: userImg, name: 'Jony Ive', lastMessage: 'Hey', lastMessageTime: '12 Dec', unreadedMessages: 0
+    },
+    {
+        avatar: userImg, name: 'Jony Ive', lastMessage: 'Hey', lastMessageTime: '12 Dec', unreadedMessages: 0
+    },
+]
