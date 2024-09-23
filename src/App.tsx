f49c358bd2f2781054cfd5d9fda7565179021903
nmthong226 <nmthong226@gmail.com>
2024-09-23 09:38:11 +0700

    build(mock-api): apply fetch user data, mocking api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import Layout from './layout/layout'
 import ChatPage from './pages/ChatPage'
+import { mainUser } from './assets/constant';
+
+
+// Fetch dummy data from constant files, mocking API call
+const fetchDummyUserData = () => {
+  return mainUser
+};
 
 function App() {
+  const [userData, setUserData] = useState<User | null>(null);
+  // mocking API call for user data
+  useEffect(() => {
+    const fetchedData = fetchDummyUserData();
+    setTimeout(() => {
+      setUserData(fetchedData);
+    }, 400); // 1 second delay
+  }, []);
+
+
   return (
-    <Layout>
-      <ChatPage />
+    <Layout user={userData}>
+      <ChatPage user={userData} />
     </Layout>
   )
 }
