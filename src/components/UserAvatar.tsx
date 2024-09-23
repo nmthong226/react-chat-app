d480a21a18cde651ffd2b6f94b996cfe0d32d493
nmthong226 <nmthong226@gmail.com>
2024-09-23 09:39:37 +0700

    build: apply dynamic user info

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,14 +1,18 @@
+import { FC } from 'react';
 import clsx from 'clsx';
-import userImg from '/user-avatar.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const UserAvatar = ({ className }: { className?: string }) => {
-  const userAvatar = userImg; //dummy user avatar
-  const userName = 'John Doe'; //dummy user name
+type UserAvatarProps = {
+  userAvatar?: string;
+  userName?: string;
+  className?: string;
+};
+
+const UserAvatar: FC<UserAvatarProps> = ({ userAvatar, userName, className }) => {
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
-      <img src={userAvatar} alt="user avatar" className={clsx('w-20 h-20 rounded-full border-4 border-white hover:cursor-pointer', className)} />
+      <img src={userAvatar} alt="user avatar" className={clsx('w-20 h-20 rounded-full border-4 border-white hover:cursor-pointer', className)} loading="eager"/>
       <div className='flex flex-row items-center space-x-2 hover:cursor-pointer'>
         <span className="text-lg text-zinc-700 font-semibold">{userName}</span>
         <FontAwesomeIcon icon={faChevronDown} className='size-3'/>
