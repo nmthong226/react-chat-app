7842e8ef00e5bdf520c175ef9cdc88dccaa08cf3
nmthong226 <nmthong226@gmail.com>
2024-09-23 09:39:52 +0700

    build: apply dynamic user info

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -5,18 +5,19 @@ import Header from "../components/Header";
 
 type TProps = {
     children: React.ReactNode;
+    user: User | null;
 };
 
-const Layout: FC<TProps> = ({ children }) => {
+const Layout: FC<TProps> = ({ children, user }) => {
     return (
         <div className="flex flex-row bg-boxBg dark:bg-zinc-900 min-h-screen">
             {/* Sidebar */}
-            <SideBar />
+            <SideBar user={user}/>
             {/* Main Content */}
             <div className={clsx("flex flex-col min-h-full container w-full flex-1 py-8 px-20")}>
                 <Header />
                 <main className="flex-1">
-                    {children}
+                    {children || <p>No content available.</p>}
                 </main>
             </div>
         </div>
